9cae0cf53c4fe514e11b6a5f8f8b71c35cd5e6a5
Ali Kri <alinanosovets6849@gmail.com>
2023-01-05 12:57:18 +0100

    almost finsished with preview component visual representation

--- src/components/doctor-preview/doctor-preview.component.jsx
+++ src/components/doctor-preview/doctor-preview.component.jsx
@@ -5,7 +5,7 @@ import network from "../assets/network.svg";
 const DoctorPreview = (props) => {
 	return (
 		<div className="doc-preview-container">
-			<div className="img-container-docp">
+			<div className="doc-img-container-docp">
 				<img src={docsIcon} alt="" />
 				<div className="network-container-docp"> 
 					<div><img src={network} alt="" /></div>
@@ -13,12 +13,15 @@ const DoctorPreview = (props) => {
 				</div>
 			</div>
 			<div className="doc-info-container-docp">
+				<div className="top-border-container"> 
 				<h2>Marry Hill, MD</h2>
 				<p>Dermatologist</p>
 				<div className="ratings-doc-preview">
 					<img src={star} alt="" />
 					<p>4,65 <span>(345)</span></p>
 				</div>
+				</div>
+				
 				<div className="gender-container-docp">
 					<div className="gender-bold">Gender:
 						<p>Female</p>
@@ -32,11 +35,11 @@ const DoctorPreview = (props) => {
 					</div>
 				</div>
 			</div>
-			<div>
+			<div className="doc-contact-container-docp">
 				<div className="location-container-docp">
 					<h3>Main location:</h3>
-					<p>305 7th Ave, Fl 10 New York, NY 10001 <span>0.7 miles</span></p>
-					<p>(400) 453-4533</p>
+					<p>305 7th Ave, Fl 10 New York, NY 10001 • <span>0.7 miles</span></p>
+					<p className="phone-docp"><span>Contact:</span> (400) 453-4533</p>
 				</div>
 				<div className="facility-container-docp">
 					<h3>Facility:</h3>
