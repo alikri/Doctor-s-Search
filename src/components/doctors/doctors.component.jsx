8199a5c8c0383346b59af9d8396e7aeeea385ded
Ali Kri <alinanosovets6849@gmail.com>
2023-01-10 13:32:06 +0100

    fixed height of thefilters onthe main page and added loading bar to the doc list page

--- src/components/doctors/doctors.component.jsx
+++ src/components/doctors/doctors.component.jsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import DoctorPreview from "../doctor-preview/doctor-preview.component";
 import loop from "../assets/loopToSearch.svg";
 import AsideFilters from "../aside-filters/aside-filters.component";
+import loader from "../assets/icons8-iphone-spinner.gif";
 
 const Doctors = () => {
 
@@ -19,8 +20,8 @@ const Doctors = () => {
 	useEffect(() => {
 		fetch("https://woyllyhb24txpvnuetgcn4lgw40pgmbc.lambda-url.eu-central-1.on.aws/?page=1&pageSize=30")
 			.then(response => response.json())
-			.then(data => setDoctors(data.results));
-	
+			.then(data => setDoctors(data.results))
+			.catch(err => alert("Server issue.", err));
 	}, []);
 
 	useEffect(() => {
@@ -79,7 +80,7 @@ const Doctors = () => {
 					>Filter Options</button>
 				</div>
 				{
-					doctors &&
+					doctors ?
 					<div>
 						<div className="search-doc-list">
 							<input
@@ -105,7 +106,7 @@ const Doctors = () => {
 						<div className="doc-list-page-msg">
 							<h2> {filteredDocs && filteredDocs.length} options within 20 miles of {zipcode}</h2>
 						</div>
-					</div>
+					</div> : <img className="loader-doc-list" src={loader} alt="Loading.." />
 				}
 				<div className="doc-big-container">
 					{doctorList}
