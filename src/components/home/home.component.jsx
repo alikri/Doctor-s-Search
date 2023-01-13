bf462d3623d9f1b4c75807da2cf64a127658dd7a
Dzmitry_Krivolap <krivolap.dima@gmail.com>
2023-01-13 23:56:44 +0100

    sync

---
+++ src/components/home/home.component.jsx
@@ -0,0 +1,28 @@
+import "./home.styles.scss";
+import image from "../assets/home-background.png";
+import SearchBar from "../search bar/searchBar.component";
+import FilterNav from "../filter-nav/filter-nav.component";
+import Doctors from "../doctors/doctors.component";
+import DoctorProfile from "../doctor-profile/doctor-profile.component";
+
+import { Routes, Route } from "react-router-dom";
+
+const Home = () => {
+
+	const myStyles = {
+		backgroundImage: `url(${image})`,
+	}
+	return (
+		<div className="home-container" style={myStyles} >
+
+			<Routes>
+				<Route index element={<><FilterNav /><SearchBar/></>} />
+				<Route path="/doctors" element={<Doctors />} />
+				<Route path="/doctors/:docId" element={<DoctorProfile/>} />
+            </Routes>
+		</div>
+
+	)
+}
+
+export default Home;
