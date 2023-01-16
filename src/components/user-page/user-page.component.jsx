9eb28b95e88d40aa7e9668aff3ce5e0602e4d2b3
Ali Kri <alinanosovets6849@gmail.com>
2023-01-16 15:47:14 +0100

    added controlled inputs to the user page component

--- src/components/user-page/user-page.component.jsx
+++ src/components/user-page/user-page.component.jsx
@@ -1,7 +1,32 @@
 import "./user-page.styles.scss";
-import whitePlaceholder from "../assets/user-pic-placeholder.png"
+import whitePlaceholder from "../assets/user-pic-placeholder.png";
+import { useState } from "react";
 
 const UserPage = () => {
+	const [edit, setEdit] = useState(false);
+	const [userDataSavedByUser, setUserDataSavedByUser] = useState({
+		userFisrtName: "",
+		userLastName: "",
+		userInsurance: "",
+		userInsurancePlan: "",
+		userEmail: "",
+		userCurrentPassword: "",
+		userUpdatePassword: ""
+	})
+	console.log(userDataSavedByUser);
+
+	const handleEditBtn = () => {
+		setEdit(prevValue => !prevValue);
+	}
+	const handleUserInputChange = (e) => {
+		setUserDataSavedByUser(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        })
+	}
+
 	return (
 		<div className="user-page-container">
 			<div className="top-container">
@@ -13,12 +38,17 @@ const UserPage = () => {
 						<img src={whitePlaceholder} alt="" width="150px" height="150px"/>
 					</div>
 					<div className="user-info">
-						<div><h2>Edit / Save</h2></div>
+						<div>
+							<h2 onClick={handleEditBtn}>{edit ? "Edit" : "Save"}</h2>
+						</div>
 						<div className="change-user-info">
-							<input type="text" placeholder="Fisrt Name"/>
-							<input type="text" placeholder="Last Name"/>
-							<input type="text" placeholder="Insurance"/>
-							<input type="text" placeholder="Insurance Plan"/>
+							<input name="userFisrtName" value={userDataSavedByUser.userFisrtName} onChange={handleUserInputChange} readOnly={edit} type="text" placeholder="Fisrt Name"/>
+							<input name="userLastName" value={userDataSavedByUser.userLastName} onChange={handleUserInputChange} readOnly={edit} type="text" placeholder="Last Name"/>
+							<input name="userInsurance" value={userDataSavedByUser.userInsurance} onChange={handleUserInputChange} readOnly={edit} type="text" placeholder="Insurance" />
+							<input name="userInsurancePlan"value={userDataSavedByUser.userInsurancePlan} onChange={handleUserInputChange} readOnly={edit} type="text" placeholder="Insurance Plan" />
+							<input name="userChangeEmail" value={userDataSavedByUser.userChangePassword} onChange={handleUserInputChange}readOnly={edit} type="email" placeholder="Change Email" />
+							<input name="userCurrentPassword" value={userDataSavedByUser.userCurrentPassword} onChange={handleUserInputChange} readOnly={edit} type="password" placeholder="Current Password" />
+							<input name="userUpdatePassword" value={userDataSavedByUser.userUpdatePassword} onChange={handleUserInputChange} readOnly={edit} type="password" placeholder="Update Password" />
 						</div>
 					</div>
 				</div>
