f6e0632b995d26b8b80f7ff8cc214efd6e2ea2f3
Dzmitry_Krivolap <krivolap.dima@gmail.com>
2023-01-21 15:25:42 +0100

    add basic login

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import { Amplify, Auth } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './amplify-config.js';
 import './app.styles.scss';
 import Home from './routes/home-page/home.component.jsx';
 import Header from './components/header/header.component';
@@ -5,9 +10,15 @@ import Footer from './components/footer/footer.component';
 // import Doctors from './components/doctors/doctors.component';
 // import { Link, Routes, Route } from "react-router-dom";
 
-function App() {
+
+Amplify.configure(awsconfig);
+
+function App({ signOut, user }) {
 	return (
 		<div className='main-content'>
+			<h1>Hello {user.username}</h1>
+      		<button onClick={signOut}>Sign out</button>
+
 			<Header />
 			<Home />
 			<Footer />
@@ -16,7 +27,7 @@ function App() {
 	);
 }
 
-export default App;
+export default withAuthenticator(App);
 
 
 // font-family: 'Barlow', sans-serif;
---
+++ src/amplify-config.js
@@ -0,0 +1,10 @@
+const config =  {
+    "aws_project_region": "eu-central-1",
+    "aws_cognito_identity_pool_id": "eu-central-1:14f5228e-d834-47f2-b5c0-88faf714a55d",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": "eu-central-1_ixyKQQPRo",
+    "aws_user_pools_web_client_id": "1t2i1sm4l8t6ht7dtb1iion4mp"
+};
+
+
+export default config;
